e3907bff15635fea29fcb20e508a581f6137eb46
cober2019 <53579603+cober2019@users.noreply.github.com>
2021-08-05 07:10:59 -0400

    Add files via upload

--- src/Components/interfaceCard.js
+++ src/Components/interfaceCard.js
@@ -12,8 +12,7 @@ export  function InterfaceCard(props){
   const interfacesRef = React.createRef()
   const arpTableRef = React.createRef()
   const table = TableHtml(arpTableRef)
-  
-  
+
   useEffect(() => {
     if(chart){
       let updatedChart = UpdateChart(chart, parseInt(props.value['statistics']['tx-kbps']),parseInt(props.value['statistics']['rx-kbps']));
@@ -27,7 +26,8 @@ export  function InterfaceCard(props){
 
   useEffect(() => {
     if(chartStatus !== true){
-      var chart = InitialChartBuild(interfacesRef.current.getContext('2d'), parseInt(props.value['statistics']['tx-kbps']), parseInt(props.value['statistics']['rx-kbps'])); 
+      var chart = InitialChartBuild(interfacesRef.current.getContext('2d'), parseInt(props.value['statistics']['tx-kbps']), parseInt(props.value['statistics']['rx-kbps']));
+
       setChart(chart)
       setArpCount(props.arps.length)
       setChartStatus(true)
@@ -79,6 +79,8 @@ export  function InterfaceCard(props){
                   <div className="row">
                     <div className="col-6">
                       <p className="card-text">Name: {props.value.name}</p>
+                      <p className="card-text">Speed: {parseInt(props.value.speed) / 1e6 }</p>
+                      <p className="card-text">Status: {props.value['oper-status']}</p>
                       <p className="card-text">IP: {props.value.ipv4}-{props.value['ipv4-subnet-mask']}</p>
                       <p className="card-text">Descr: {props.value.description}</p>
                       <p className="card-text">MTU: {props.value.mtu}</p>
