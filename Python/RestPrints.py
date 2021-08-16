9b655b9a22f941b7e22ee729b3e120ce9fada919
cober2019 <53579603+cober2019@users.noreply.github.com>
2021-08-16 11:34:44 -0400

    Update RestPrints.py

--- Python/RestPrints.py
+++ Python/RestPrints.py
@@ -1,7 +1,72 @@
 """Collections of functions that retrive data from a cisco device"""
 
+from json.decoder import JSONDecodeError
+import requests
+import json
+import time
+import os
+import warnings
+warnings.filterwarnings('ignore', message='Unverified HTTPS request')
+
+headers = {"Content-Type": 'application/yang-data+json', 'Accept': 'application/yang-data+json'}
+
+def get_interfaces(ip, port, username, password, ex_down=None):
+    """Gets real time interface statistics using IOS-XE\n
+        Cisco-IOS-XE-interfaces-oper:interfaces and live arp data via Cisco-IOS-XE-arp-oper:arp-data/arp-vrf"""
+
+    data = {}
+    interface_data = {}
+
+    try:
+        uri = f"https://{ip}:{port}/restconf/data/Cisco-IOS-XE-interfaces-oper:interfaces"
+
+        response = requests.get(uri, headers=headers, verify=False, auth=(username, password))
+        converted_json = json.loads(response.text)
+        interface_data = converted_json.get('Cisco-IOS-XE-interfaces-oper:interfaces').get('interface')
+    except (JSONDecodeError, requests.exceptions.ConnectionError, requests.exceptions.InvalidURL,UnboundLocalError, AttributeError):
+        pass
+    
+    if interface_data:
+        try:
+            uri = f"https://{ip}:{port}/restconf/data/Cisco-IOS-XE-arp-oper:arp-data/arp-vrf"
+            response = requests.get(uri, headers=headers, verify=False, auth=(username, password))
+
+            converted_json = json.loads(response.text, strict=False)
+            get_keys = dict.fromkeys(converted_json)
+            parent_key = list(get_keys.keys())[0]
+
+            for interface in interface_data:
+                entries = []
+                convert_bandwidth = _convert_to_mbps(interface)
+                for i in converted_json[parent_key]:
+                    try:
+                        for entry in i.get('arp-oper'):
+                            print(entry)
+                            if entry.get('interface') == interface.get('name'):
+                                entry.pop('interface')
+                                entry['time'] = entry.get('time').split('.')[0].strip('T00')
+                                entry.append(entry)
+                    except TypeError:
+                        pass
+
+                data[interface.get('name')] = {'interface': interface.get('name'), 'data': convert_bandwidth, 'arps': entries}
+                
+        except (JSONDecodeError, requests.exceptions.ConnectionError, requests.exceptions.InvalidURL,UnboundLocalError, AttributeError):
+            # No arp entries, device error
+            for interface in interface_data:
+                convert_bandwidth = _convert_to_mbps(interface)
+                data[interface.get('name')] = {'interface': interface.get('name'), 'data': convert_bandwidth, 'arps': []}
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(f"{'Interface':<33} {'Status':<15} {'Mbps In':<10}{'Mbps Out':<10}{'PPS In':<10} {'PPS Out':<12}{'MTU':<12}{'IP':<22} {'Mask':<1}")
+    print("-------------------------------------------------------------------------------------------------------------------------------------------")
+    for v in data.values():
+        [_print_interfaces_extended(data, ex_down) for data in v.values() if isinstance(data, dict)]
+
+    return data
+
 def get_switch(ip, port, username, password):
-    """Gets device components /restconf/data/openconfig-platform:components"""
+    """Gets device trunks and vlan from a cisco device"""
 
     data = {}
     trunk =[]
