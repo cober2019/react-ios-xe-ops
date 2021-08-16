c1c47c9e58ac6a01f112c3812dc5f14a85c5968f
cober2019 <53579603+cober2019@users.noreply.github.com>
2021-08-16 12:57:16 -0400

    Update RestPrints.py

--- Python/RestPrints.py
+++ Python/RestPrints.py
@@ -10,6 +10,81 @@ warnings.filterwarnings('ignore', message='Unverified HTTPS request')
 
 headers = {"Content-Type": 'application/yang-data+json', 'Accept': 'application/yang-data+json'}
 
+def get_interfaces(ip, port, username, password, ex_down=None) -> dict:
+    """Gets real time interface statistics using IOS-XE\n
+        Cisco-IOS-XE-interfaces-oper:interfaces and live arp data via Cisco-IOS-XE-arp-oper:arp-data/arp-vrf"""
+
+    data = {}
+    interface_data = {}
+
+    try:
+        uri = f"https://{ip}:{port}/restconf/data/Cisco-IOS-XE-interfaces-oper:interfaces"
+
+        response = requests.get(uri, headers=headers, verify=False, auth=(username, password))
+        converted_json = json.loads(response.text)
+        interface_data = converted_json.get('Cisco-IOS-XE-interfaces-oper:interfaces').get('interface')
+    except (JSONDecodeError, requests.exceptions.ConnectionError, requests.exceptions.InvalidURL,UnboundLocalError, AttributeError):
+        pass
+    
+    if interface_data:
+        try:
+            uri = f"https://{ip}:{port}/restconf/data/Cisco-IOS-XE-arp-oper:arp-data/arp-vrf"
+            response = requests.get(uri, headers=headers, verify=False, auth=(username, password))
+
+            converted_json = json.loads(response.text, strict=False)
+            get_keys = dict.fromkeys(converted_json)
+            parent_key = list(get_keys.keys())[0]
+
+            for interface in interface_data:
+                entries = []
+                convert_bandwidth = _convert_to_mbps(interface)
+                for i in converted_json[parent_key]:
+                    try:
+                        for entry in i.get('arp-oper'):
+                            print(entry)
+                            if entry.get('interface') == interface.get('name'):
+                                entry.pop('interface')
+                                entry['time'] = entry.get('time').split('.')[0].strip('T00')
+                                entry.append(entry)
+                    except TypeError:
+                        pass
+
+                data[interface.get('name')] = {'interface': interface.get('name'), 'data': convert_bandwidth, 'arps': entries}
+                
+        except (JSONDecodeError, requests.exceptions.ConnectionError, requests.exceptions.InvalidURL,UnboundLocalError, AttributeError):
+            # No arp entries, device error
+            for interface in interface_data:
+                convert_bandwidth = _convert_to_mbps(interface)
+                data[interface.get('name')] = {'interface': interface.get('name'), 'data': convert_bandwidth, 'arps': []}
+
+    print(f"{'Interface':<33} {'Status':<15} {'Mbps In':<10}{'Mbps Out':<10}{'PPS In':<10} {'PPS Out':<12}{'MTU':<12}{'IP':<22} {'Mask':<1}")
+    print("-------------------------------------------------------------------------------------------------------------------------------------------")
+    for v in data.values():
+        [_print_interfaces_extended(data, ex_down) for data in v.values() if isinstance(data, dict)]
+
+    return data
+
+def _print_interfaces_extended(data, ex_down) -> dict:
+    """Prints interface data"""
+
+    if ex_down == True:
+        if data.get('oper-status', {}) == 'up':
+            print(f"{data.get('name', {}):<35}{data.get('oper-status', {}):<15} {int(data.get('statistics', {}).get('rx-kbps')) / 1000:<10}{int(data.get('statistics', {}).get('tx-kbps')) / 1000:<10}{data.get('statistics', {}).get('rx-pps'):<10} {data.get('statistics', {}).get('tx-pps'):<10}{data.get('mtu'):<10}{data.get('ipv4', ''):<20} {data.get('ipv4-subnet-mask', ''):<1}")
+    else:
+        print(f"{data.get('name', {}):<35}{data.get('oper-status', {}):<15} {int(data.get('statistics', {}).get('rx-kbps')) / 1000:<10}{int(data.get('statistics', {}).get('tx-kbps')) / 1000:<10}{data.get('statistics', {}).get('rx-pps'):<10} {data.get('statistics', {}).get('tx-pps'):<10}{data.get('mtu'):<10}{data.get('ipv4', ''):<20} {data.get('ipv4-subnet-mask', ''):<1}")
+
+def _convert_to_mbps(interface):
+    """Convert Kbps to Mbps"""
+
+    interface['statistics']['tx-kbps'] = int(interface.get('statistics').get('tx-kbps')) / 1000
+    interface['statistics']['rx-kbps'] = int(interface.get('statistics').get('tx-kbps')) / 1000
+    if interface['oper-status'] == 'if-oper-state-ready':
+        interface['oper-status'] = 'up'
+    else:
+        interface['oper-status'] = 'down'
+
+    return interface
+
 def get_vlans(ip, port, username, password) -> list:
     """Gets device components /restconf/data/openconfig-platform:components"""
 
