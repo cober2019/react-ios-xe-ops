a4ce3344b18d73195946e3cf30850d4861deb4de
cober2019 <53579603+cober2019@users.noreply.github.com>
2021-08-20 02:08:39 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Index }  from './Components/index'
 import { LayerTwo }  from './Components/layerTwo'
+import { DeviceAuth }  from './Components/login'
 import {
   BrowserRouter as Router,
   Switch,
@@ -10,13 +11,34 @@ import {
 
 
 function App() {
+
+  const setAuthTrue = async (ip, username, password, port) => {
+    console.log(localStorage.getItem('ip'))
+    if(localStorage.getItem('ip') === null){
+      localStorage.setItem('ip', ip);
+      localStorage.setItem('username', username);
+      localStorage.setItem('password', password);
+      localStorage.setItem('port', port);
+      }
+    }
+
   return (
     <div classname="App">
       <Router>
         <Switch>
-          <Route exact path="/layerTwo" component={LayerTwo}/>
-          <Route exact path="/index" component={Index}/>
-          <Route exact path="/" component={Index}/>
+          <Route path="/layerTwo" render={props => (<LayerTwo username={localStorage.getItem('username')} 
+                                                  password={localStorage.getItem('password')} 
+                                                  ip={localStorage.getItem('ip')} 
+                                                  port={localStorage.getItem('port')} 
+                                                  />)}/>
+
+          <Route path="/index" render={props => (<Index username={localStorage.getItem('username')} 
+                                                  password={localStorage.getItem('password')} 
+                                                  ip={localStorage.getItem('ip')} 
+                                                  port={localStorage.getItem('port')} 
+                                                  />)}/>
+          <Route path="/logout" render={props => (<DeviceAuth callback={setAuthTrue} {...props}/>)}/>
+          <Route path="/" render={props => (<DeviceAuth callback={setAuthTrue} {...props}/>)}/>
         </Switch>
     </Router>
     </div>
