c565e2b077ee482cdaa5bf539b13f7a2790ead72
cober2019 <53579603+cober2019@users.noreply.github.com>
2021-09-15 13:47:30 -0400

    Add files via upload

---
+++ src/Components/LayerTwo/layerTwo.js
@@ -0,0 +1,147 @@
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import { DpNeighbors} from '../Other/dp_neighbors'
+import { GlobalSpanTreeHtml} from '../Other/chartConfigs'
+import { MacTable} from './macAddress'
+import { SpanTable} from './spanTree'
+import { Trunks} from './trunks'
+import { AccessPorts} from './accessPorts'
+import { Vlans} from './vlans'
+import { Navbar } from '../Other/navbar';
+import { ErrorBoundary } from '../Other/errorBoundry';
+
+export  function LayerTwo(props){
+  const [update, setUpdate] = useState(0)
+  const errorRef = useRef(false)
+  const bridgeGlobalTble = useRef(false)
+
+  useEffect(() => {
+
+    const source = axios.CancelToken.source()
+
+    if(update !== 0){
+
+      axios.post('/pollL2Page', 
+      {cancelToken: source.token, 
+      'ip': localStorage.getItem('ip'), 
+      'username': localStorage.getItem('username'), 
+      'password': localStorage.getItem('password'), 
+      'port': localStorage.getItem('port')},
+      {'headers': { 'Authorization': 'Bearer ' + localStorage.getItem('token')}}).then(data => {
+
+        localStorage.setItem('trunks', JSON.stringify(data.data.trunks));
+        localStorage.setItem('macs', JSON.stringify(data.data.mac_addresses));
+        localStorage.setItem('access', JSON.stringify(data.data.access));
+        localStorage.setItem('dpNeighbors', JSON.stringify(data.data.dpNeighbors));
+        localStorage.setItem('vlans', JSON.stringify(data.data.vlans));
+        localStorage.setItem('span', JSON.stringify(data.data.span));
+        bridgeGlobalTble.current = GlobalSpanTreeHtml(JSON.parse(localStorage.getItem('span'))[1]['Cisco-IOS-XE-spanning-tree-oper:stp-global'])
+        
+        errorRef.current = false
+        let render = update + 1
+        setUpdate(render)
+
+      }).catch(error => {
+          console.log(error)
+          errorRef.current = true
+          let render = update + 1
+          setUpdate(render)
+      });
+    }
+
+    return () => {
+      source.cancel()}
+
+  }, [update])
+  
+
+useEffect(() => {
+  if(update === 0){
+    localStorage.setItem('ip', props.ip);
+    localStorage.setItem('username', props.username);
+    localStorage.setItem('password', props.password);
+    localStorage.setItem('port', props.port);
+    let render = update + 1
+    setUpdate(render)
+  }
+
+      
+}, [])
+
+
+if (errorRef.current){
+  return  <div>
+            <Navbar update={update} ip={localStorage.getItem('ip')}/>
+            <h4 class="text-center fade-in" style={{marginTop: 100}}>Error Collecting Data. I'll Keep Trying</h4>
+            <div class="warning-loader text-center"></div>
+        </div>
+}
+else if (update >= 2 && !errorRef.current){
+  return <div className="container-fluid">
+          <Navbar ip={localStorage.getItem('ip')}/>
+          <div className="card border-0 mt-3 mb-3 bg-dark">
+                    <div className="card-body">
+                      <div className="row">
+                        <ul class="nav">
+                              <li class="nav-item">
+                                  <a href="#spanglobal" class="nav-link text-light show active " data-toggle="tab">Spanning Tree Global</a>
+                              </li>
+                              <li class="nav-item">
+                                  <a href="#spanports" class="nav-link text-light" data-toggle="tab">Per-Vlan Span. Details</a>
+                              </li>
+                          </ul>
+                          <div class="tab-content">
+                              <div class="tab-pane"id="spanports" role="tabpanel">     
+                              { JSON.parse(localStorage.getItem('span'))[0]['Cisco-IOS-XE-spanning-tree-oper:stp-details']['stp-detail'].map(instance => (
+                                    <ErrorBoundary>
+                                      <SpanTable span={instance}/>
+                                    </ErrorBoundary>
+                                  ))}
+                                </div>   
+                              <div class="tab-pane show active" id="spanglobal" role="tabpanel">      
+                                {bridgeGlobalTble.current}
+                             </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <ErrorBoundary>
+                        <DpNeighbors dpNeighbors={JSON.parse(localStorage.getItem('dpNeighbors'))}/>
+                      </ErrorBoundary>
+                    </div>
+                    <div className="row">
+                        <ErrorBoundary>
+                          <Vlans vlans={JSON.parse(localStorage.getItem('vlans'))}/>
+                        </ErrorBoundary>
+                        <div className="col-xl-5">
+                          <div className="row">
+                            <ErrorBoundary>
+                              <Trunks ports={JSON.parse(localStorage.getItem('trunks'))}/>
+                            </ErrorBoundary>
+                          </div>
+                          <div className='row'>
+                            <ErrorBoundary>
+                              <AccessPorts ports={JSON.parse(localStorage.getItem('access'))}/>
+                            </ErrorBoundary>
+                          </div>
+                            <div className='row'>
+                            <ErrorBoundary>
+                              <MacTable macs={JSON.parse(localStorage.getItem('macs'))}/>
+                            </ErrorBoundary>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+}
+else if (update < 2){
+  return  <div>
+            <h4 class="text-center fade-in" style={{marginTop: 100}}>Collecting L2 data for {localStorage.getItem('ip')}</h4>
+            <div class="loader text-center"></div>
+        </div>
+}
+
+}
+    
+  
+  
---
+++ src/Components/Other/navbar.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import { ApiCheck } from './promises'
+import { Link } from "react-router-dom";
+
+export  function Navbar(props){
+  const [apiStatus, setApiStatus] = useState(true)
+
+  useEffect(() => {
+    (async () => {
+      try{
+        let apiStatus = await ApiCheck()
+        if(apiStatus.status === 200)
+            setApiStatus(true)
+        else{
+            setApiStatus(false)
+        }
+    }
+    catch{
+        setApiStatus(false)
+    }
+
+    })()
+    }, [props.update])
+
+  return  <div>
+            <nav class="navbar navbar-expand-lg navbar-light" style={{backgroundColor: "black"}}>
+                <div class="container-fluid">
+                    <a class="navbar-brand"  style={{color: 'white'}}>Bandwidth Monitor | </a>
+                    <div>
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <Link className="nav-items" style={{paddingLeft:15 , paddingRight: 15, fontSize: 20, color: "white", textDecoration: 'none'}} to="/index">Home</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link className="nav-items" style={{fontSize: 20, color: "white", textDecoration: 'none'}} to="/layerTwo">LayerTwo</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link className="nav-items" style={{paddingLeft:20, fontSize: 20 , color: "white", textDecoration: 'none'}} to="/routing">Routing</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link className="nav-items" style={{paddingLeft:15, fontSize: 20, color: "white", textDecoration: 'none'}} to="/environment">Env</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link className="nav-items" style={{paddingLeft:15, fontSize: 20, paddingRight: 850, color: "white", textDecoration: 'none'}} to="/config">Cfg/Oper Browser</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link style={{fontSize: 18, color: "white", textDecoration: 'none'}} className="nav-items" to='/logout'>| Logout({props.ip})</Link>
+                            </li>
+                            
+                        </ul>            
+                    </div>
+                    {apiStatus ? <div class="fa fa-circle env-row-text nav-items"><span style={{fontWeight: 'normal', paddingLeft: 5}}>API Reachable</span></div> 
+                        :  
+                        <div class="fa fa-circle env-row-text-warn nav-items"><span style={{fontWeight: 'normal', paddingLeft: 5}}>API Unreachable</span></div>}
+                </div>
+            </nav>                      
+            </div>
+  }
+  
