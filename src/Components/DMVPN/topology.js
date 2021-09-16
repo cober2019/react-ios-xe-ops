d7d09f0d33cf61bcda912475dd8d875bf65b9cd7
cober2019 <53579603+cober2019@users.noreply.github.com>
2021-09-16 16:17:04 -0400

    Add files via upload

---
+++ src/Components/DMVPN/topology.js
@@ -0,0 +1,111 @@
+import router from '../Images/router.png';
+import { Network } from "vis-network";
+
+var font = { size: 15, strokeWidth: 0, color: 'white'}
+var arrowEnabled = {enabled: true}
+var nodeFont = { size: 16, color: "white"}
+var options = {
+  nodes: {
+    shape: 'image',
+    image: router,
+    size: 35,
+    color: {
+      border: 'black',
+      background: '#166F20',
+      highlight: {
+        border: '#2B7CE9',
+        background: '#D2E5FF'
+          }
+        }
+      },
+    layout: {
+      randomSeed: undefined,
+      improvedLayout:true,
+      clusterThreshold: 200,
+      hierarchical: {
+        enabled:true,
+        levelSeparation: 500,
+        treeSpacing: 300,
+        edgeMinimization: true,
+        parentCentralization: true,
+        direction: 'DU',        // UD, DU, LR, RL
+        sortMethod: 'hubsize',  // hubsize, directed
+        shakeTowards: 'roots'  // roots, leaves
+          }
+      },
+      physics:{
+      enabled: true,
+      hierarchicalRepulsion: {
+        centralGravity: 0.0,
+        springLength: 0,
+        springConstant: 0.01,
+        nodeDistance: 250,
+        damping: 1,
+        avoidOverlap: 1
+      },
+    },
+
+}
+
+export function UpdateDmvpnTopology(ref, peers, localIp){
+           
+    Object.entries(peers).forEach((details, i) => {
+      if (details[1].state !== 'UP'){
+        ref.body.data.edges.push({id: details[1].peerNbma, 
+                        from: localIp, 
+                        to: i, 
+                        color: 'yellow',
+                        label: details[1].state + '\n' + details[1].peerTunnel + "\n" + details[1].attrb, 
+                        font: font
+                        });
+            }
+            else{
+              ref.body.data.edges.push({id: details[1].vlanInt, 
+                    from: localIp, 
+                    to: i, 
+                    color: 'green',
+                    label: details[1].state + '\n' + details[1].peerTunnel + "\n" + details[1].attrb, 
+                    font: font
+                    });
+            }})
+    
+    return  ref
+
+    }
+
+export function DmvpnTopologyBuild(ref, peers, localIp){
+
+    const nodes = []
+    const edges = []
+
+    nodes.push({id: localIp, label: 'Local' + '\n' + localIp, font: nodeFont })
+
+    Object.entries(peers).forEach((details, i) => {
+        nodes.push({id: details[0], label: details[1].peerNbma, font: font});
+    })
+
+    Object.entries(peers).forEach((details, i) => {
+          if (details[1].state !== 'UP'){
+                edges.push({id: details[1].peerNbma, 
+                  from: localIp, 
+                  to: i, 
+                  color: 'yellow',
+                  label: details[1].state + '\n' + details[1].peerTunnel + "\n" + details[1].attrb, 
+                  font: font
+                  });
+          }
+          else{
+              edges.push({id: details[1].peerNbma, 
+                  from: localIp, 
+                  to: i, 
+                  color: 'green',
+                  label: details[1].state + '\n' + details[1].peerTunnel + "\n" + details[1].attrb, 
+                  font: font
+                  });
+          }})
+          console.log(ref)
+          var network = new Network(ref, {nodes: nodes, edges: edges}, options);
+    
+    return  network
+
+    }
---
+++ src/Components/LayerTwo/layerTwo.js
@@ -0,0 +1,113 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+import { DpNeighbors} from '../Other/dp_neighbors'
+import { GlobalSpanTreeHtml} from '../Other/chartConfigs'
+import { MacTable} from './macAddress'
+import { SpanTable} from './spanTree'
+import { Trunks} from './trunks'
+import { AccessPorts} from './accessPorts'
+import { Vlans} from './vlans'
+import { Navbar } from '../Other/navbar';
+import { ErrorBoundary } from '../Other/errorBoundry';
+
+export  function LayerTwo(props){
+  const bridgeGlobalTble = useRef(false)
+  const { isLoading, error, data } = useQuery('pollL2Page', async () => {
+    
+      const response = await axios.post('/pollL2Page',{'ip': localStorage.getItem('ip'), 'username': localStorage.getItem('username'), 
+      'password': localStorage.getItem('password'), 'port': localStorage.getItem('port')}, {'headers': { 'Authorization': 'Bearer ' + localStorage.getItem('token')}})
+      
+      bridgeGlobalTble.current = GlobalSpanTreeHtml(response.data.span[1]['Cisco-IOS-XE-spanning-tree-oper:stp-global'])
+      return response.data
+
+      },
+      {
+        refetchInterval: 5000,
+      }
+  )
+
+useEffect(() => {
+    localStorage.setItem('ip', props.ip);
+    localStorage.setItem('username', props.username);
+    localStorage.setItem('password', props.password);
+    localStorage.setItem('port', props.port);  
+}, [])
+
+
+if (error){
+  return  <div>
+            <Navbar update={data} ip={localStorage.getItem('ip')}/>
+            <h4 class="text-center fade-in" style={{marginTop: 100}}>Error Collecting Data. I'll Keep Trying</h4>
+            <div class="warning-loader text-center"></div>
+        </div>
+}
+else if (data){
+  return <div className="container-fluid">
+          <Navbar update={data} ip={localStorage.getItem('ip')}/>
+          <div className="card border-0 mt-3 mb-3 bg-dark">
+                    <div className="card-body">
+                      <div className="row">
+                        <ul class="nav">
+                              <li class="nav-item">
+                                  <a href="#spanglobal" class="nav-link text-light show active " data-toggle="tab">Spanning Tree Global</a>
+                              </li>
+                              <li class="nav-item">
+                                  <a href="#spanports" class="nav-link text-light" data-toggle="tab">Per-Vlan Span. Details</a>
+                              </li>
+                          </ul>
+                          <div class="tab-content">
+                              <div class="tab-pane"id="spanports" role="tabpanel">     
+                              { data.span[0]['Cisco-IOS-XE-spanning-tree-oper:stp-details']['stp-detail'].map(instance => (
+                                    <ErrorBoundary>
+                                      <SpanTable span={instance}/>
+                                    </ErrorBoundary>
+                                  ))}
+                                </div>   
+                              <div class="tab-pane show active" id="spanglobal" role="tabpanel">      
+                                {bridgeGlobalTble.current}
+                             </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <ErrorBoundary>
+                        <DpNeighbors dpNeighbors={data.dpNeighbors}/>
+                      </ErrorBoundary>
+                    </div>
+                    <div className="row">
+                        <ErrorBoundary>
+                          <Vlans vlans={data.vlans}/>
+                        </ErrorBoundary>
+                        <div className="col-xl-5">
+                          <div className="row">
+                            <ErrorBoundary>
+                              <Trunks ports={data.trunks}/>
+                            </ErrorBoundary>
+                          </div>
+                          <div className='row'>
+                            <ErrorBoundary>
+                              <AccessPorts ports={data.access}/>
+                            </ErrorBoundary>
+                          </div>
+                            <div className='row'>
+                            <ErrorBoundary>
+                              <MacTable macs={data.macs}/>
+                            </ErrorBoundary>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+}
+else if (isLoading){
+  return  <div>
+            <h4 class="text-center fade-in" style={{marginTop: 100}}>Collecting L2 data for {localStorage.getItem('ip')}</h4>
+            <div class="loader text-center"></div>
+        </div>
+}
+
+}
+    
+  
+  
