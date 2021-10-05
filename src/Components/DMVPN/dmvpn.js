12af9fa5e59ce0744f58b141e18df118608670a4
cober2019 <53579603+cober2019@users.noreply.github.com>
2021-10-04 23:07:16 -0400

    Update dmvpn.js

--- src/Components/DMVPN/dmvpn.js
+++ src/Components/DMVPN/dmvpn.js
@@ -46,7 +46,7 @@ else if (data){
 }
 else if (isLoading){
     return  <div>
-                <h4 class="text-center fade-in" style={{marginTop: 100}}>Collecting DMVPN Toplogy Data From {localStorage.getItem('ip')}</h4>
+                <h4 class="text-center fade-in" style={{marginTop: 100}}>Collecting DMVPN Topology Data From {localStorage.getItem('ip')}</h4>
                 <div class="loader text-center"></div>
             </div>
 }
