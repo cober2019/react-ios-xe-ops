af240fe035449d34133a31a8009a02b2bd0ae231
cober2019 <53579603+cober2019@users.noreply.github.com>
2021-10-05 11:35:19 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ ReactDOM.render(
         <App />
       </RecoilRoot>
   </React.StrictMode>,
-   document.getElementById('root') || document.createElement('div')
+   document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
