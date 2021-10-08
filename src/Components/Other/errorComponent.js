d89b0a183e75a24117cde0b4a682c2c91b1130f3
cober2019 <53579603+cober2019@users.noreply.github.com>
2021-10-08 12:40:49 -0400

    Update errorComponent.js

--- src/Components/Other/errorComponent.js
+++ src/Components/Other/errorComponent.js
@@ -6,8 +6,7 @@ export function IsErrorFallback(){
 
     return <Card bg="dark" className="mb-3" style={{height: '500px'}}>
             <Card.Body>
-            <h4 class="text-center fade-in" style={{marginTop: 150}}>Error Collecting Data. I'll Keep Trying</h4>
-                <div class="warning-loader text-center"></div>
+               <div class="warning-loader text-center" style={{marginTop: 175}}/>
             </Card.Body>
         </Card>
 }
