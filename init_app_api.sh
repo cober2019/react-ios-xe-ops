0b5c0c2b83c266cb732f7ece35d2b4e4c1168ea9
cober2019 <53579603+cober2019@users.noreply.github.com>
2021-10-12 12:55:26 -0400

    Update init_app_api.sh

--- init_app_api.sh
+++ init_app_api.sh
@@ -5,7 +5,7 @@ npm install package.json -y
 pm2 --name XE-Ops start npm -- start
 cd "$(pwd)/BackEndModeules"
 python3.8 -m venv ios-xe-ops-env
-source ios-xe-ops-envv/bin/activate 
+source ios-xe-ops-env/bin/activate 
 pip3 install -r requirements.txt 
 python3 api_routes.py 
 
