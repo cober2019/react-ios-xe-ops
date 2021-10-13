bb1ccd01474ccf9565601b32a21eee22a7c4c87f
cober2019 <53579603+cober2019@users.noreply.github.com>
2021-10-13 17:55:55 -0400

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,7 @@
+
+#!/bin/bash
+
+#IF YOU DONT HAVE THE FOLLOWING DEPENDENCIES INSTALLED THEN RUN THIS SCRIPT, sudo bash install_dependencies
 sudo apt update
 INSTALL_PKGS="python3.10 nodejs npm openssl python3.8-venv pip"
 for i in $INSTALL_PKGS; do
