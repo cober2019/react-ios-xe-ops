9ea7fce83ddd68e4f3436e137545d1c89c9119af
cober2019 <53579603+cober2019@users.noreply.github.com>
2021-10-15 11:55:26 -0400

    Update wsgi.py

--- BackEndModeules/wsgi.py
+++ BackEndModeules/wsgi.py
@@ -1,4 +1,9 @@
-from run import app
+from api_routes import app
+import os
+import ssl
+
+ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+ctx.load_cert_chain(f'{os.getcwd()}/domainame.crt', f'{os.getcwd()}/domainame.key')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(ssl_context=ctx)
