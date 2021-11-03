6c359e63f5332def71a4157c4342a7f56c5eb15b
cober2019 <53579603+cober2019@users.noreply.github.com>
2021-11-03 14:22:17 -0400

    Add files via upload

---
+++ src/App.test.js
@@ -0,0 +1,6 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+it('xeops', () => {
+  render(<App />);
+});
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App.js';
+import { RecoilRoot, atom } from 'recoil';
+
+ReactDOM.render(
+        <App />,
+  document.getElementById('root')
+);
+
+
+
